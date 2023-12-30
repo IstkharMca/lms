59a6da32a9285824a194fd3abc3ef219c45befde
Istkhar Ali <istkharali1991@gmail.com>
2023-12-31 01:54:43 +0530

    admin dashboard setup

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,10 +4,33 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function AdminDashboard() {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('admin/login');
+    }
+
+    public function AdminLogin() {
+        return view('admin.admin_login');
+    }
+
+    public function AdminProfile() {
+        $profile_data = User::find(Auth::id());
+        return view('admin.admin_profile_view',compact('profile_data'));
     }
 }
